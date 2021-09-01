d9db1634d1d7764aa5c9d43751c66d3babe03d23
fanxs1998 <318463482@qq.com>
2021-09-01 16:36:01 +0800

    新增导航栏和轮播图组件

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,15 +6,11 @@
 </template>
 
 <script>
-  import TabBar from "../components/common/tabbar/TabBar";
-  import TarBarItem from "../components/common/tabbar/TabBarItem";
-  import MainTabBar from "../components/content/mainTabbar/MainTabBar";
+  import MainTabBar from "components/content/mainTabbar/MainTabBar";
   export default {
     name: "Home",
     components: {
       MainTabBar,
-      TarBarItem,
-      TabBar
     }
   }
 </script>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,45 @@
 <template>
-  <div>首页</div>
+  <div id="home">
+    <nav-bar class="home-nav"><div slot="center">首页</div> </nav-bar>
+
+    <home-swiper :banners="banners"/>
+  </div>
 </template>
 
 <script>
+  import NavBar from "components/common/navbar/NavBar";
+  import HomeSwiper from "./childComponents/HomeSwiper";
+  
+  import {getHomeMultidata} from "api/home"
+  
   export default {
-    name: "Home"
+    name: "Home",
+    components: {
+      NavBar,
+      HomeSwiper
+    },
+    
+    data() {
+      return {
+        banners: [],
+        recommend: []
+      }
+    },
+    
+    created() {
+      getHomeMultidata().then(res => {
+        console.log(res);
+        this.banners = res.data.banner.list
+        this.recommends = res.data.recommends
+        console.log(this.banners);
+      })
+    }
   }
 </script>
 
 <style scoped>
-
+  .home-nav {
+    background-color: var(--color-tint);
+    color: #fff;
+  }
 </style>
