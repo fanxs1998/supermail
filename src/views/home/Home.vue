4dac8abfcec76b997d6ba3d924b91b7ed1088531
fanxs1998 <318463482@qq.com>
2021-09-02 11:27:24 +0800

    新增recommend推荐图、feature细节图、tabcontrol业务组件

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,20 +1,77 @@
 <template>
   <div id="home">
     <nav-bar class="home-nav"><div slot="center">首页</div> </nav-bar>
-
     <home-swiper :banners="banners"/>
+    <home-recommend :recommends="recommends"></home-recommend>
+    <home-feature/>
+    <tab-control :titles="['流行','新款','精选']"
+                 class="tab-control"/>
+    
+    <ul>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+    </ul>
   </div>
 </template>
 
 <script>
-  import NavBar from "components/common/navbar/NavBar";
   import HomeSwiper from "./childComponents/HomeSwiper";
+  import HomeRecommend from "./childComponents/HomeRecommend";
+  import HomeFeature from "./childComponents/HomeFeature";
   
+  import NavBar from "components/common/navbar/NavBar";
+  import TabControl from "../../components/content/tabControl/TabControl";
+
   import {getHomeMultidata} from "api/home"
   
   export default {
     name: "Home",
     components: {
+      TabControl,
+      HomeFeature,
+      HomeRecommend,
       NavBar,
       HomeSwiper
     },
@@ -22,7 +79,7 @@
     data() {
       return {
         banners: [],
-        recommend: []
+        recommends: []
       }
     },
     
@@ -30,16 +87,30 @@
       getHomeMultidata().then(res => {
         console.log(res);
         this.banners = res.data.banner.list
-        this.recommends = res.data.recommends
+        this.recommends = res.data.recommend.list
         console.log(this.banners);
+        console.log(this.recommends[0].link);
       })
     }
   }
 </script>
 
 <style scoped>
+  #home {
+    padding-top: 44px;
+  }
   .home-nav {
     background-color: var(--color-tint);
     color: #fff;
+    
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    z-index: 9;
+  }
+  .tab-control {
+    position: sticky;
+    top: 44px;
   }
 </style>
